e7dfc21f2058ee425e96ec5f7a8cba5e4a50c1b9
Dhruv Ranjan <dhruvranjansharma@gmail.com>
2025-08-25 19:26:38 +0530

    Initial commit

---
+++ src/test/java/com/movie/management/movieManagement/MovieManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.management.movieManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
